34cbda9bf8669415f2092ff6501ce2076131addf
test1 <2899531592@qq.com>
2025-05-30 16:36:07 +0800

    改进分布式锁，满足： 1.在获取锁时存入线程标识 2.在释放锁时先获取锁中的线程标识，判断是否与当前线程标识一致

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,17 +1,20 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
 
     private String name;
-    private static final String KEY_PREFIX = "lock:";
     private StringRedisTemplate stringRedisTemplate;
+    private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -20,16 +23,21 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public boolean tryLock(long timeoutSec) {
-        //获取线程标识
-        long threadId = Thread.currentThread().threadId();
+        //获取线程标识(uuid-33)
+        String threadId = ID_PREFIX + Thread.currentThread().threadId();
 
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId+"", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return BooleanUtil.isTrue(success);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        String threadId = ID_PREFIX + Thread.currentThread().threadId();
+        //获取锁中的标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if(threadId.equals(id)){
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
